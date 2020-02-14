525083022e3999807729c340d6743608db37c23e
rbbrioso28 <rbbrioso28@gmail.com>
2020-02-14 15:06:13 +0800

    Tool creator config for laravel

---
+++ ReadMe.md
@@ -0,0 +1,8 @@
+# Melis Tool Creator Laravel
+This repository contains the Tool Creator configuration to activate Laravel option on
+the selection in the Tool Creator.
+
+# Clone project
+```bash
+git clone https://github.com/melisplatform/melis-tool-creator-laravel.git
+```
---
+++ src/Module.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Melis Technology (http://www.melistechnology.com)
+ *
+ * @copyright Copyright (c) 2015 Melis Technology (http://www.melistechnology.com)
+ *
+ */
+
+namespace MelisToolCreatorLaravel;
+
+use Zend\Mvc\ModuleRouteListener;
+use Zend\Mvc\MvcEvent;
+use Zend\ModuleManager\ModuleManager;
+use Zend\Session\Container;
+use Zend\Stdlib\ArrayUtils;
+
+class Module
+{
+    public function onBootstrap(MvcEvent $e)
+    {
+        $eventManager        = $e->getApplication()->getEventManager();
+        $moduleRouteListener = new ModuleRouteListener();
+        $moduleRouteListener->attach($eventManager);
+    }
+
+    public function getConfig()
+    {
+        $config = array();
+        $configFiles = array(
+            include __DIR__ . '/../config/module.config.php',
+            include __DIR__ . '/../config/toolcreator.config.php',
+        );
+
+        foreach ($configFiles as $file)
+            $config = ArrayUtils::merge($config, $file);
+
+        return $config;
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+}
